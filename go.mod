67ecca5ba581415a487f606a7bae8d274c426e96
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-22 00:34:26 -0500

    Get go-chi package, godotenv package

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/runquan-ray-zhou/rss-aggregator-project
 
 go 1.22.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	fmt.Println("Hello World")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment") //environment variable does not live in shell session
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
