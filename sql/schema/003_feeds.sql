ed06da0a8e47617f617a639d213aaef6aab69810
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-28 00:16:05 -0500

    Add rss file

--- sql/schema/003_feeds.sql
+++ sql/schema/003_feeds.sql
@@ -5,7 +5,7 @@ CREATE TABLE feeds(
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL,
-    url TEXT UNIQUE NOT NULL,
+    url TEXT NOT NULL UNIQUE,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
